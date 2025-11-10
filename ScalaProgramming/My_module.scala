2cde4cdd9714278a7eb91048684aae1cbc088cc3
Meku-nint <mw908334@gmail.com>
2025-11-10 09:44:57 +0300

    object and class in scala

--- ScalaProgramming/My_module.scala
+++ ScalaProgramming/My_module.scala
@@ -1,10 +1,42 @@
-object My_module{// 
+// Class definition
+class IntroductionToScala {
+  def aboutScala(about: String): String = {
+    s"Scala is all about $about"
+  }
 
-def abs(n:Int):Int={
-    if (n<0) -n
+  def usedFor(usage: String): String = {
+    s"Scala is used for $usage"
+  }
+}
+
+// Singleton object
+object MyModule {
+  
+  // Immutable values
+  val aboutScala: String = "functional programming"
+  val usage: String = "building scalable applications"
+
+  // Create an instance of IntroductionToScala
+  val scalaInfo: IntroductionToScala = new IntroductionToScala()
+
+  // Print class information
+  println(scalaInfo.aboutScala(aboutScala))
+  println(scalaInfo.usedFor(usage))
+
+  // Method to calculate absolute value
+  def abs(n: Int): Int = {
+    if (n < 0) -n
     else n
-}
-def main(args:Array[String]):Unit={
-    println("absolute value of -10 is :" +abs(-10))
-}
+  }
+
+  // Private helper method to format absolute value
+  private def formatAbs(x: Int): String = {
+    val msg: String = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  // Main method - program entry point
+  def main(args: Array[String]): Unit = {
+    println(formatAbs(-42))
+  }
 }
